a2b1b80775f4866a9be6bed7a388b09aa7cb2a3b
nathiss <kamil.jakub.rusin@gmail.com>
2020-08-04 20:20:42 +0200

    Create HTTP status-code functionality

---
+++ Http.Tests/StatusCode/StatusCodeRepositoryTests.cs
@@ -0,0 +1,88 @@
+#region Copyrights
+// This file is a part of the Http project.
+//
+// Copyright (c) 2020 Kamil Rusin
+// Licensed under the MIT License.
+// See LICENSE.txt file in the project root for full license information.
+#endregion
+
+using Http.StatusCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Http.Tests.StatusCode
+{
+    [TestClass]
+    public class StatusCodeRepositoryTests
+    {
+        [DataTestMethod]
+        [DataRow(100)]
+        [DataRow(101)]
+        [DataRow(200)]
+        [DataRow(201)]
+        [DataRow(202)]
+        [DataRow(203)]
+        [DataRow(204)]
+        [DataRow(205)]
+        [DataRow(206)]
+        [DataRow(300)]
+        [DataRow(301)]
+        [DataRow(302)]
+        [DataRow(303)]
+        [DataRow(304)]
+        [DataRow(305)]
+        [DataRow(306)]
+        [DataRow(307)]
+        [DataRow(400)]
+        [DataRow(401)]
+        [DataRow(402)]
+        [DataRow(403)]
+        [DataRow(404)]
+        [DataRow(405)]
+        [DataRow(406)]
+        [DataRow(407)]
+        [DataRow(408)]
+        [DataRow(409)]
+        [DataRow(410)]
+        [DataRow(411)]
+        [DataRow(412)]
+        [DataRow(413)]
+        [DataRow(414)]
+        [DataRow(415)]
+        [DataRow(416)]
+        [DataRow(417)]
+        [DataRow(426)]
+        [DataRow(500)]
+        [DataRow(501)]
+        [DataRow(502)]
+        [DataRow(503)]
+        [DataRow(504)]
+        [DataRow(505)]
+        public void Get_GivenValidStatusCodeNumber_ReturnsObjectConstaingTheSameNumber(int statusCodeNumber)
+        {
+            // Arrange
+            var repository = new StatusCodeRepository();
+
+            // Act
+            var statusCode = repository.Get(statusCodeNumber);
+
+            // Assert
+            Assert.AreEqual(statusCodeNumber, statusCode.Value);
+        }
+
+        [DataTestMethod]
+        [DataRow(102)]
+        [DataRow(-8)]
+        [DataRow(0)]
+        [DataRow(100000000)]
+        [DataRow(506)]
+        [DataRow(308)]
+        public void Get_GivenInvalidStatusCodeNumber_ThrownsAnExceptionOfTypeUnknownStatusCodeException(int statusCode)
+        {
+            // Arrange
+            var repository = new StatusCodeRepository();
+
+            // Assert
+            Assert.ThrowsException<UnknownStatusCodeException>(() => repository.Get(statusCode));
+        }
+    }
+}
