7d57425f088ff93568c4729f509e5525869d6bb7
nathiss <kamil.jakub.rusin@gmail.com>
2020-11-10 17:07:20 +0100

    Add missing UTs for HttpRequestParser

--- Http.Tests/Http11/Request/HttpRequestParserTests.cs
+++ Http.Tests/Http11/Request/HttpRequestParserTests.cs
@@ -25,13 +25,15 @@ namespace Http.Tests.Http11.Request
 
         private static readonly IHttpVersionRepository _httpVersionRepository = new HttpVersionRepository();
 
+        private RequestBuilder _requestBuilder;
+
         private IRequestParser _requestParser;
 
         [TestInitialize]
         public void SetUp()
         {
-            var requestBuilder = new RequestBuilder(_httpMethodRepository, _httpVersionRepository, new HttpHeaders());
-            _requestParser = new HttpRequestParser(requestBuilder);
+            _requestBuilder = new RequestBuilder(_httpMethodRepository, _httpVersionRepository, new HttpHeaders());
+            _requestParser = new HttpRequestParser(_requestBuilder);
         }
 
         [TestMethod]
@@ -90,7 +92,7 @@ namespace Http.Tests.Http11.Request
         public void Status_GivenValidRequestLine_ReturnsUnsatisfied()
         {
             // Arrange
-            var strData = "GET /index.html HTTP/1.1\r\n";
+            const string strData = "GET /index.html HTTP/1.1\r\n";
             var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
 
             // Act
@@ -104,7 +106,7 @@ namespace Http.Tests.Http11.Request
         public void Status_GivenLowercaseMethod_ReturnsError()
         {
             // Arrange
-            var strData = "get /index.html HTTP/1.1\r\n";
+            const string strData = "get /index.html HTTP/1.1\r\n";
             var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
 
             // Act
@@ -118,7 +120,7 @@ namespace Http.Tests.Http11.Request
         public void Status_GivenRequestLineWithoutCRLF_ReturnsUnsatisfied()
         {
             // Arrange
-            var strData = "GET /index.html HTTP/1.1";
+            const string strData = "GET /index.html HTTP/1.1";
             var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
 
             // Act
@@ -132,7 +134,233 @@ namespace Http.Tests.Http11.Request
         public void Status_GivenRequestLineWithoutTarget_ReturnsError()
         {
             // Arrange
-            var strData = "GET HTTP/1.1\r\n";
+            const string strData = "GET HTTP/1.1\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_NoMethod_ReturnsError()
+        {
+            // Arrange
+            const string strData = "/index.html HTTP/1.1\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_NoHttpVersion_ReturnsError()
+        {
+            // Arrange
+            const string strData = "OPTIONS /index.html\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_TooLongMethod_ReturnsError()
+        {
+            // Arrange
+            const string strData = "12345678 /index.html HTTP/1.1\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_UnknownMethod_ReturnsError()
+        {
+            // Arrange
+            const string strData = "ERASE /index.html HTTP/1.1\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_NoHeadersSection_ReturnReady()
+        {
+            // Arrange
+            const string strData = "GET /index.html HTTP/1.1\r\n\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Ready, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_ValidHeaderSection_ReturnsReady()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Host:example.com\r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Ready, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_FieldValueWithLeadingWhiteSpace_ReturnsReady()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Host:                       example.com\r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Ready, _requestParser.Status);
+            Assert.IsTrue(_requestBuilder.HasHeader("Host"));
+            Assert.AreEqual("example.com", _requestBuilder.GetHeader("Host"));
+        }
+
+        [TestMethod]
+        public void Status_FieldValueWithTrailingWhiteSpace_ReturnsReady()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Host:example.com                  \r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Ready, _requestParser.Status);
+            Assert.IsTrue(_requestBuilder.HasHeader("Host"));
+            Assert.AreEqual("example.com", _requestBuilder.GetHeader("Host"));
+        }
+
+        [TestMethod]
+        public void Status_NoRequestLine_ReturnsError()
+        {
+            // Arrange
+            const string strData =
+                "Host:example.com                  \r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_NoColonInHeader_ReturnsError()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Hostexample.com                  \r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_FieldNameIsEmpty_ReturnsError()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                ":example.com                  \r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Error, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_FieldValueIsEmpty_ReturnsReady()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Host:\r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Ready, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_FieldValueContainsOnlySpaces_ReturnsReady()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Host:                 \r\n" +
+                "\r\n";
+            var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
+
+            // Act
+            _requestParser.FeedData(data);
+
+            // Assert
+            Assert.AreEqual(ParserStatus.Ready, _requestParser.Status);
+        }
+
+        [TestMethod]
+        public void Status_FieldValueContainsLF_ReturnsError()
+        {
+            // Arrange
+            const string strData =
+                "GET /index.html HTTP/1.1\r\n" +
+                "Host: example\n.com\r\n" +
+                "\r\n";
             var data = new List<byte>(Encoding.ASCII.GetBytes(strData));
 
             // Act
